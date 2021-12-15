76afcb66f06df80b0cf2faa0f1d228dbb4841c97
robcabobb <robcabobb@gmail.com>
2021-12-15 12:06:43 -0500

    added form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEL = document.querySelector("#task-form");
 var tasksToDoEL = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEL.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEL.addEventListener("submit", createTaskHandler);
